e5282e98e97b36336613af3ac4b25387f687482b
quangChe <quang.n.che@gmail.com>
2019-05-08 14:43:56 -0700

    Utility API Methods
    Added a getLocation function to retrieve user's current geolocation,
    which is used in the getFeed function to list a default list of nearest
    businesses serving happy hour.

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 const ApiUrl = 'http://localhost:9000';
 
+const getCurrentLocation = () => new Promise(async resolve => {
+  const cb = (pos) => resolve(pos.coords);
+  const err = () => resolve({latitude: 34.0522, longitude: -118.2437});
+  return await navigator.geolocation.getCurrentPosition(cb, err)
+})
+
 const Yelp = {
-  get: async () => {
+  getFeed: async () => {
     try {
-      const request = await axios.get(ApiUrl);
+      const {latitude, longitude} = await getCurrentLocation();
+      const search = encodeURIComponent(`term=happy+hour&longitude=${longitude}&latitude=${latitude}`);
+      const request = await axios.get(`${ApiUrl}/list/businesses?search=${search}`)
       return request.data;
     } catch (err) {
       console.log(err);
