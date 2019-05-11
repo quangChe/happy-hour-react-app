907cc87c52cc6d10736416cf29b929903d8094e8
quangChe <quang.n.che@gmail.com>
2019-05-10 23:57:38 -0700

    Wired up redux-thunk to the app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducers from './store/reducers';
+import thunk from 'redux-thunk';
 
 import Navbar from './components/NavbarComponent';
 import HomeScreen from './components/HomeScreen';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 function App() {
   return (
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
       <Navbar/>
       <HomeScreen/> 
     </Provider>
