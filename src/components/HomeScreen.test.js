40e727e86986e7850d5524ece9729d5864d46104
quangChe <quang.n.che@gmail.com>
2019-05-14 22:08:50 -0700

    Set up basic tests for components

--- src/components/HomeScreen.test.js
+++ src/components/HomeScreen.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import StoreProvider from './store'
+import HomeScreen from './HomeScreen';
+import StoreProvider from '../store';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<StoreProvider><App/></StoreProvider>, div);
+  ReactDOM.render(<StoreProvider><HomeScreen/></StoreProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/NavbarComponent.test.js
+++ src/components/NavbarComponent.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import StoreProvider from './store'
+import NavbarComponent from './NavbarComponent';
+import StoreProvider from '../store'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<StoreProvider><App/></StoreProvider>, div);
+  ReactDOM.render(<StoreProvider><NavbarComponent/></StoreProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
