89aba39fd4c52f2e237ff17a52ed1ce9614477bf
quangChe <quang.n.che@gmail.com>
2019-05-15 17:50:44 -0700

    HomeScreen Cleanup
    Cleaned up the HomeScreen component and rendered the newly-built
    VenuesCarousel component in it.
    
    TODO: Further style changes to VenuesCarousel

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,36 +2,19 @@ import React from 'react';
 import './HomeScreen.scss';
 import { connect } from 'react-redux';
 import { fetchNearby } from '../store/actions';
-
-import api from '../utils/apis';
+import VenuesCarousel from './VenuesCarousel';
 
 class Home extends React.Component {
-  async componentDidMount() {
+  componentDidMount() {
     this.props.fetchNearby();
-
-    const test = await api.searchId('tTafiXZvcXziFjpu4zxO7Q');
-    console.log('****', test);
   }
 
   render() {
-    const width = window.innerWidth;
     const { nearbyBusinesses } = this.props;
-
+    const randomNumber = Math.round(Math.random() * 1000000);
 
     return (
-      
-      // TODO: Refactor this to a separate component
-      <div className="container">
-        <h1>Nearby</h1>
-        { nearbyBusinesses.map(business => 
-            <div key={business.id} style={{width, backgroundImage: `url(${business.image_url})`}} className="business-card">
-              <div onClick={() => console.log(business)} className="card-overlay">
-                <h2>{business.name}</h2>
-              </div>
-            </div>
-          )   
-        }
-      </div>
+      <VenuesCarousel id={randomNumber} venues={nearbyBusinesses}/>
     )
   }
 }
