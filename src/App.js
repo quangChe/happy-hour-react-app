141a0adabc02a7c61b7cee437003ceb84bebddd2
quangChe <quang.n.che@gmail.com>
2019-05-17 16:43:18 -0700

    Connected and tested react-router-dom on app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import HomeScreen from './components/HomeScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+const TestPath = () => <div>Hello?</div>
 
 function App() {
   return (
-    <HomeScreen/> 
+    <Router>
+      <Route path="/" exact component={HomeScreen}/>
+      <Route path="/test" component={TestPath}/>
+    </Router>
   )
 }
 
