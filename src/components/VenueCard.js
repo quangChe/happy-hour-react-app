393418f73380020e4d5f67866368e9d072a8b57a
quangChe <quang.n.che@gmail.com>
2019-05-16 23:12:54 -0700

    Added a VenueCard component to app

---
+++ src/components/VenueCard.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './VenueCard.scss';
+
+class VenueCard extends React.Component {
+  
+  render() {
+    const { handleClick, venue, width } = this.props;
+    const cardStyle = {
+      width,
+      backgroundImage: `url(${venue.image_url})`
+    };
+
+    return (
+      <div className="venue-card"
+          style={cardStyle} >
+        <div onClick={() => handleClick(venue.id)} className="card-overlay">
+          <p className="name">{venue.name}</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default VenueCard;
