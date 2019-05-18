d6021d58ce59780016aa0c02883c18de3ec4ec50
quangChe <quang.n.che@gmail.com>
2019-05-18 00:49:39 -0700

    Set up HashRouter to more easily serve the app from Node server,'happy-hour-api'

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import HomeScreen from './components/HomeScreen';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 const TestPath = () => <div>Hello?</div>
+const Error = () => <div>404!!</div>
 
 function App() {
   return (
     <Router>
       <Route path="/" exact component={HomeScreen}/>
       <Route path="/test" component={TestPath}/>
+      <Route path="*" component={Error}/>
     </Router>
   )
 }
