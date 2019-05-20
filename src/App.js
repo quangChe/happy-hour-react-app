5034ca68a626e033c5af7caf8208cb98461a5bb0
quangChe <quang.n.che@gmail.com>
2019-05-20 16:50:59 -0700

    Moved Navbar component inside the Router and added switch to router to provide a 404 catch-all route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HomeScreen from './components/HomeScreen';
-import { HashRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/NavbarComponent';
 
 const TestPath = () => <div>Hello?</div>
 const Error = () => <div>404!!</div>
@@ -8,9 +10,12 @@ const Error = () => <div>404!!</div>
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={HomeScreen}/>
-      <Route path="/test" component={TestPath}/>
-      <Route path="*" component={Error}/>
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={HomeScreen}/>
+        <Route path="/test" component={TestPath}/>
+        <Route component={Error}/>
+      </Switch>
     </Router>
   )
 }
