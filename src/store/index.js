b5a184e2ace5b6a5624fe1990997e42d15be8dff
quangChe <quang.n.che@gmail.com>
2019-05-19 23:43:00 -0700

    Configured app to with Redux's compose utility to make it compatible with Redux Dev Tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-export const Store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const Store = createStore(
+  reducers, 
+  composeEnhancers(applyMiddleware(thunk))
+);
