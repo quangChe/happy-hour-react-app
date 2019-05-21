5ffc23f920f7eff270844844f9a4eb72d6b4d58c
quangChe <quang.n.che@gmail.com>
2019-05-21 14:11:12 -0700

    Updated the LoadingSpinner component to use the Logo component

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './LoadingSpinner.scss';
 
+import Logo from './Logo';
+
 const LoadingSpinner = () => (
   <div className="container">
     <div className="lds-ripple"><div></div><div></div></div>
+    <Logo/>
   </div>
 )
 
