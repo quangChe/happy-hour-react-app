cdb0b626831c7b9506bdc2d96a5f37a9a4a358af
quangChe <quang.n.che@gmail.com>
2019-05-21 14:09:47 -0700

    Created a Logo component to provide a reusable logo throughout app

---
+++ src/components/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Logo.scss';
+
+const Logo = () => (
+  <div className="logo-container">
+    <svg className="logo" xmlns="http://www.w3.org/2000/svg" width="32" height="32" x="0" y="0"  version="1.1" viewBox="0 0 32 32">
+      <path fill="rgb(236, 123, 0)" d="M24.5 13.5c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z" coriginal="rgb(164, 255, 190, 1)" colortype="4" awdzp="4" colorover="rgb(236, 123, 0)"/>
+      <path fill="#dad3b8" d="M24.5 3C21.46 3 19 5.46 19 8.5s2.46 5.5 5.5 5.5S30 11.54 30 8.5 27.54 3 24.5 3zm0 10c-2.48 0-4.5-2.02-4.5-4.5S22.02 4 24.5 4 29 6.02 29 8.5 26.98 13 24.5 13z" coriginal="rgb(77, 81, 82, 1)" colortype="16" awdzp="16"/>
+      <path fill="transparent" d="M23.35 9.35l-7.5 7.5c-.81.82-1.49 1.15-2.35 1.15-.85 0-1.55-.34-2.35-1.15l-7.5-7.5c-.15-.14-.19-.35-.11-.54S3.8 8.5 4 8.5h19c.2 0 .38.12.46.31s.04.4-.11.54z" coriginal="rgb(172, 240, 242, 1)" colortype="1" awdzp="1" colorover="rgb(213, 206, 179)"/>
+      <path id="MTBteHk0ODkxMDAwMDAwMDAwMHh6czkwMw" fill="rgb(241, 128, 0)" d="M13.5 18c-.846 0-1.55-.343-2.354-1.146l-3.5-3.5c-.143-.143-.186-.358-.108-.545.077-.188.26-.31.462-.31h11c.202 0 .385.122.462.31s.034.4-.108.544l-3.5 3.5C15.04 17.668 14.357 18 13.5 18z" coriginal="rgb(255, 128, 237, 1)" colortype="3" awdzp="3" colorover="rgb(241, 128, 0)"/>
+      <path fill="#dad3b8" d="M23.71 9.71c.28-.29.37-.72.21-1.09C23.77 8.24 23.4 8 23 8H4c-.4 0-.77.24-.92.62-.16.37-.07.8.21 1.09l8.74 8.74c1.04 1.04.97 1.55.97 2.05V28H6.5c-.28 0-.5.22-.5.5s.22.5.5.5h14c.28 0 .5-.22.5-.5s-.22-.5-.5-.5H14v-7.5c0-.5-.11-.98.95-2.03l8.76-8.76zM4 9h19l-3 3H7L4 9zm9.5 8.5c-.62 0-1.21-.21-2-1L8 13h11l-3.5 3.5c-.79.79-1.36 1-2 1z" coriginal="rgb(77, 81, 82, 1)" colortype="16" awdzp="16"/>
+    </svg>
+    <h1 className="title">SixtySips</h1>
+  </div>
+)
+
+export default Logo;
