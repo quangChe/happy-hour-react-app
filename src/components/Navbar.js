f017c05041d6c22c02420cf00ff3d3051b6606c7
quangChe <quang.n.che@gmail.com>
2019-05-22 23:17:37 -0700

    Navbar Changes and NavMenu
    Made Navbar a stateful component to monitor NavButton clicks states.
    This will be used later to toggle a NavMenu component. Stylistic updates
    were also made to the Navbar in preparation for this upcoming component.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,16 +7,41 @@ import { Link } from 'react-router-dom';
 import NavButton from './NavButton';
 import Logo from './Logo';
 
-const Navbar = (props) => props.loading 
-  ? null
-  : (
-    <div id="container" style={{color: colors.black}}>
-      <Link to="/" style={{textDecoration: 'none'}}>
-        <Logo/>
-      </Link>
-      <NavButton open={false}/>
-    </div>
-  );
+class Navbar extends React.Component {
+
+  state = {
+    menuOpen: false,
+  }
+
+  openNavMenu = (open) => {
+    this.setState({menuOpen: open})
+  }
+
+  render() {
+    const { loading } = this.props;
+    const { menuOpen } = this.state;
+
+    const menuStyles = {
+      opacity: menuOpen ? 1 : 0,
+      transition: 'opacity 1s ease-in-out',
+    }
+
+
+    return loading
+      ? null
+      : (
+        <div id="navigation-container">
+          { menuOpen && <div className="nav-menu" style={{menuStyles}}></div> }
+          <div className="navbar" style={{color: colors.black}}>
+            <Link to="/" style={{textDecoration: 'none'}}>
+              <Logo/>
+            </Link>
+            <NavButton onClick={this.openNavMenu} open={false}/>
+          </div>
+        </div>
+      );
+  }
+}
 
 const mapStateToProps = ({appLoading}) => {
   return {loading: appLoading};
