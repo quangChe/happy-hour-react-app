c71206b2773c74944c82e6e578a972a8c8e68a72
quangChe <quang.n.che@gmail.com>
2019-05-25 17:19:51 -0700

    Refactored the NavBar test for improved structure.

--- src/components/NavBar/NavBar.test.js
+++ src/components/NavBar/NavBar.test.js
@@ -1,34 +1,29 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Provider from '../../store/Provider';
-import NavBar from './NavBar';
+
+import { NavBar } from './NavBar';
 import NavSlider from './NavSlider/NavSlider';
 import NavButton from './NavButton/NavButton';
 
 configure({adapter: new Adapter()});
 
 describe('<NavBar />', () => {
-  const LoadingNavBar = (<Provider><NavBar loading={true}/></Provider>);
-  const LoadedNavBar = (<Provider><NavBar loading={false} /></Provider>)
+  let wrapper;
 
-  it('should render no <NavSlider /> if loading state is true', () => {
-    const wrapper = shallow(LoadingNavBar);
+  beforeEach(() => {
+    wrapper = shallow(<NavBar />);
+  })
+  
+  it('Should not render <NavButton /> or <NavSlider /> if app is loading', () => {
+    wrapper.setProps({loading: true});    
+    expect(wrapper.find(NavButton)).toHaveLength(0);
     expect(wrapper.find(NavSlider)).toHaveLength(0);
   });
-
-  it('should render <NavSlider /> if loading state is false', () => {
-    const wrapper = shallow(LoadedNavBar);
-    expect(wrapper.find(NavSlider)).toHaveLength(0);
-  });
-
-  it('should render no <NavButton /> if loading state is true', () => {
-    const wrapper = shallow(LoadingNavBar);
-    expect(wrapper.find(NavButton)).toHaveLength(0);
-  });
-
-  it('should render <NavButton /> if loading state is false', () => {
-    const wrapper = shallow(LoadedNavBar);
-    expect(wrapper.find(NavButton)).toHaveLength(0);
+  
+  it('Should render <NavButton /> and <NavSlider /> if app is done loading', () => {
+    wrapper.setProps({loading: false});    
+    expect(wrapper.find(NavButton)).toHaveLength(1);
+    expect(wrapper.find(NavSlider)).toHaveLength(1);
   });
 })
