93990fe532b1ed3a1c9812cdc3fea281cd499254
quangChe <quang.n.che@gmail.com>
2019-05-28 15:50:23 -0700

    Modified action creators to provide more data in actions to accommodate changes to businesses reducers

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -2,11 +2,10 @@ import api from '../../utils/apis';
 
 export const fetchNearby = () => async dispatch => {
   const response = await api.searchNearby();
-  dispatch({ type: 'FETCH_NEARBY', payload: response});
-  dispatch({ type: 'DATA_AVAILABLE', payload: false});
+  dispatch({ type: 'FETCH_NEARBY', payload: response, loading: false});
 }
 
 export const fetchById = (id) => async dispatch => {
   const response = await api.searchId(id);
-  return dispatch( {type: 'ID_BUSINESS', payload: response})
+  return dispatch({type: 'ID_BUSINESS', payload: response})
 }
