eff5c6b5defaa1cec5a1dfc58ee13911c920fbd4
quangChe <quang.n.che@gmail.com>
2019-06-19 17:04:01 -0700

    Updated VenueCardList test to ensure that all tests in the app's test suite are passing

--- src/components/VenueCardList/VenueCardList.test.js
+++ src/components/VenueCardList/VenueCardList.test.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import { configure, shallow, mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import VenueCardList from './VenueCardList';
-import VenueCard from './VenueCard/VenueCard';
-
-configure({adapter: new Adapter()});
 
 describe('<VenueCardList />', () => {
 
-  const testState = {
-    width: 400,
-    scrollPosition: 0,
-  };
-
-  const testProps = {
+  const props = {
     id: '12345',
     venues: [
       {id: 1, name: 'Test 1', image_url: 'https://via.placeholder.com/150'},
@@ -28,13 +20,10 @@ describe('<VenueCardList />', () => {
   let wrapper;
 
   beforeEach(async () => {
-    const { venues, id, venueClick } = testProps;
-    wrapper = mount(<VenueCardList />);
-    await wrapper.setState(testState);
-    await wrapper.setProps({venues, id, venueClick});
+    wrapper = mount(<VenueCardList {...props} />);
   })
 
-  it('Should render a <VenuCard /> for each item in venues prop', () => {
-    expect(wrapper.find(<VenueCard />)).toHaveLength(3);
+  it('Should render a <VenueCard /> for each item in venues prop', () => {
+    expect(wrapper.find('VenueCard')).toHaveLength(3);
   });
 })
