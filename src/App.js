b14295d2020d218046cdbd9a0105716522cce170
quangChe <quang.n.che@gmail.com>
2020-02-26 00:14:03 -0800

    Feat: LoginForm Component
    Added login form component with placeholder stuff for now. I currently
    have it loaded on the /login path.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { observer, inject } from 'mobx-react'
 import { Route, Switch }  from 'react-router-dom'
 import Home from './pages/Home/Home';
+import LoginForm from './components/LoginForm/LoginForm';
 import NavBar from './components/NavBar/NavBar';
 import Error from './pages/Error/Error';
 
@@ -18,6 +19,7 @@ class App extends React.Component {
         <NavBar/>
         <Switch>
           <Route path="/" exact component={Home}/>
+          <Route path="/login" exact component={LoginForm}/>
           <Route component={Error}/>
         </Switch>
       </>
