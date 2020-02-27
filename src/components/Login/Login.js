c69c50d37c395d315e5e1dfec8ab3861d26cc56b
quangChe <quang.n.che@gmail.com>
2020-02-26 22:43:47 -0800

    Added a Login component and lowered height of NavBar component so that Login component renders on top

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,31 @@
-import React from 'react'
-import { observer, inject } from 'mobx-react'
+import React from 'react';
+import { observer, inject } from 'mobx-react';
+import './Login.scss';
 
-export default inject('businessStore')(observer((props) => {
-    console.log(props.businessStore.loading);
+export default inject('formStore')(observer((props) => {
+    const { loginForm } = props.formStore;
+
+    const generateFormField = (inputLabel) => (
+        <div>
+            <label htmlFor={loginForm.$(inputLabel).id}>
+                {loginForm.$(inputLabel).label}
+            </label>
+            <input {...loginForm.$(inputLabel).bind()} />
+            <p>{loginForm.$(inputLabel).error}</p>
+        </div>
+    )
 
     return (
-        <div>
-            <h1>Login works!</h1>
+        <div className="login-form">
+            <form  onSubmit={loginForm.LoginFormonSubmit}>
+                {generateFormField('email')}
+                {generateFormField('password')}
+                {generateFormField('passwordConfirm')}
+                <button type="submit" onClick={loginForm.onSubmit}>Submit</button>
+                <button type="button" onClick={loginForm.onClear}>Clear</button>
+                <button type="button" onClick={loginForm.onReset}>Reset</button>
+            </form>
         </div>
+
     )
 }));
